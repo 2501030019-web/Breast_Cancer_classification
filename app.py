b28bc9a83e7d76ee29444af3bee9299aa41a14e6
2501030019-web <2501030019@cgu-odisha.ac.in>
2026-03-04 11:55:51 +0530

    Update image in app.py to new medical banner

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ st.write("")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("medical.jpg", use_container_width=True)
+    st.image("medical-banner-with-doctor-wearing-equipment (1).jpg", use_container_width=True)
 )
 
 with col2:
